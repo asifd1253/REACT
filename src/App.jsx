ef6f9b448f4d406939a18cad93113b9d8a8b9f72
Asif <asifd@gmail.com>
2026-09-15 13:05:37 +0530

    EPISODE-08: Created the Class based component in About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { createBrowserRouter } from "react-router";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { Outlet } from "react-router";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import Error from "./components/Error";
+import Body from "./components/Body";
+import RestaurantMenu from "./components/RestaurantMenu.jsx";
 
 const App = () => {
   return (
@@ -10,5 +16,31 @@ const App = () => {
     </div>
   );
 };
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/restaurant/:restaurantId",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
 
 export default App;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const About = () => {
-  return <div>About</div>;
-};
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      age: 23,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>My name is Asif</h1>
+        <h2>I am a software developer</h2>
+        <p>I am {this.state.age} years old</p>
+        <button
+          onClick={() => {
+            this.setState({ age: this.state.age + 1 });
+          }}
+        >
+          Increase Age
+        </button>
+      </div>
+    );
+  }
+}
 
 export default About;
